5ab321c302bb0fa1df3ed05aac10c67e6ca90127
Jesper Petersson <jesper@jpetersson.se>
2014-01-14 14:22:59 +0100

    Added Monika and Emil to the list

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,19 +1,20 @@
 var PERSON_LIST = [
-	"Fanny",
 	"Björn",
 	"Matilda",
-	"Martin",
 	"Emma",
 	"Emil",
 	"Erik",
 	"Malin",
 	"Ali",
 	"Nina",
+	"Martin",
+	"Emil Ö",
 	"Fredrik",
 	"Jesper",
 	"Petter",
-	"Johan"
+	"Johan",
+	"Monika"
 ];
 
 var INIT_DATE = new Date("3 jan 2013 09:00");
-var MISSED_OCCATIONS = 0;
+var MISSED_OCCATIONS = 1;
